ce42414963f1959adea1549aeaa75148c7671e57
Peter Matthews <peterjmatthews@me.com>
2023-04-27 10:08:21 +0100

    firebase config

---
+++ firebaseConfig.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+// import * as firebase from "firebase"
+// https://firebase.google.com/docs/web/setup#available-libraries
+//add the libaries needed - making an app and authentication
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// Your web app's Firebase configuration - from the firebase website
+const firebaseConfig = {
+  apiKey: "AIzaSyCiKn7nNRQrpfKoUR1z8JXqpzxTcRp896s",
+  authDomain: "allotment-cc7dd.firebaseapp.com",
+  projectId: "allotment-cc7dd",
+  storageBucket: "allotment-cc7dd.appspot.com",
+  messagingSenderId: "278887415667",
+  appId: "1:278887415667:web:99112784c2b8df4f760f06",
+};
+//initalise the app
+const app = initializeApp(firebaseConfig);
+//get auth in
+const auth = getAuth(app);
+//export authentication
+export { auth };
