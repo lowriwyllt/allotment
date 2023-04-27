2693f158e13c6c3d88861a5469832ca3b52b7615
Peter Matthews <peterjmatthews@me.com>
2023-04-27 10:59:51 +0100

    Add jsx compatibility to tsconfig

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const HomeScreen = () => {
+const HomeScreen = (): JSX.Element => {
   return (
     <View>
       <Text>HomeScreen</Text>
--- screens/LoginScreen.tsx
+++ screens/LoginScreen.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const LoginScreen = () => {
+const LoginScreen = (): JSX.Element => {
   return (
     <View>
       <Text>LoginScreen</Text>
