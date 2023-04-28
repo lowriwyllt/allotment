6b242e6f9408a40145fadd793886243bac058bdf
HamRoss <rh@rosshamilton.co>
2023-04-28 16:51:34 +0100

    Add windows with hardcoded data

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -1,57 +1,73 @@
 import React from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import { Calendar } from "react-native-calendars";
-
 type Marked = {
   [date: string]: {
     periods: { startingDay: boolean; endingDay: boolean; color: string }[];
   };
 };
-
+const currentYear = new Date().getFullYear();
+const currentMonth = String(new Date().getMonth() + 1).padStart(2, "0");
+const testDates = [
+  {
+    event: "sewing window",
+    fromDate: "2023-05-01",
+    toDate: "2023-05-13",
+  },
+  {
+    event: "harvesting window",
+    fromDate: "2023-05-11",
+    toDate: "2023-06-26",
+  },
+];
 const CalendarSinglePlant = (): JSX.Element => {
   const getMarked = () => {
     let marked: Marked = {};
-    for (let i = 1; i <= 10; i++) {
-      let day = i.toString().padStart(2, "0");
-      let periods = [
-        {
-          startingDay: i === 1,
-          endingDay: i === 10,
-          color: "teal",
-        },
-        ...(i >= 2 && i <= 6
-          ? [
-              {
-                startingDay: i === 2,
-                endingDay: i === 6,
-                color: "orange",
-              },
-            ]
-          : []),
-      ];
-      marked[`2022-12-${day}`] = {
-        periods,
-      };
-    }
+    let color = "";
+    testDates.forEach(({ event, fromDate, toDate }) => {
+      if (event === "sewing window") {
+        color = "blue";
+      } else if (event === "harvesting window") {
+        color = "green";
+      }
+      const from = new Date(fromDate);
+      const to = new Date(toDate);
+      const currentDate = new Date(from);
+      while (currentDate <= to) {
+        const year = currentDate.getFullYear();
+        const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+        const day = String(currentDate.getDate()).padStart(2, "0");
+        if (!marked[`${year}-${month}-${day}`]) {
+          marked[`${year}-${month}-${day}`] = {
+            periods: [],
+          };
+        }
+        marked[`${year}-${month}-${day}`].periods.push({
+          startingDay: currentDate.getTime() === from.getTime(),
+          endingDay: currentDate.getTime() === to.getTime(),
+          color: color, // Set your desired color here
+        });
+        currentDate.setDate(currentDate.getDate() + 1);
+      }
+    });
     return marked;
   };
-
+  console.log(currentYear);
+  console.log(currentMonth);
   return (
     <SafeAreaView style={styles.container}>
       <Calendar
-        initialDate="2022-12-01"
+        initialDate={`${currentYear}-${currentMonth}-01`}
         markingType="multi-period"
         markedDates={getMarked()}
       />
     </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-    },
-  });
-
-  export default CalendarSinglePlant;
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+export default CalendarSinglePlant;
