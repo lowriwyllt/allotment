92370272966b02fb64dd93edba4026703caf0b29
HamRoss <rh@rosshamilton.co>
2023-05-02 10:24:31 +0100

    change hardcoded input data to days length

--- screens/Calendar.tsx
+++ screens/Calendar.tsx
@@ -8,30 +8,34 @@ type Marked = {
 };
 const currentYear = new Date().getFullYear();
 const currentMonth = String(new Date().getMonth() + 1).padStart(2, "0");
+
 const testDates = [
   {
     event: "sewing window",
     fromDate: "2023-05-01",
-    toDate: "2023-05-13",
+    numberOfDays: 10,
   },
   {
     event: "harvesting window",
-    fromDate: "2023-05-11",
-    toDate: "2023-06-26",
+    fromDate: "2023-05-08",
+    numberOfDays: 12,
   },
 ];
+
 const CalendarSinglePlant = (): JSX.Element => {
   const getMarked = () => {
     let marked: Marked = {};
     let color = "";
-    testDates.forEach(({ event, fromDate, toDate }) => {
+    testDates.forEach(({ event, fromDate, numberOfDays }) => {
       if (event === "sewing window") {
         color = "blue";
       } else if (event === "harvesting window") {
         color = "green";
       }
       const from = new Date(fromDate);
-      const to = new Date(toDate);
+      const to = new Date(
+        from.getTime() + (numberOfDays - 1) * 24 * 60 * 60 * 1000
+      );
       const currentDate = new Date(from);
       while (currentDate <= to) {
         const year = currentDate.getFullYear();
@@ -52,14 +56,64 @@ const CalendarSinglePlant = (): JSX.Element => {
     });
     return marked;
   };
-  console.log(currentYear);
-  console.log(currentMonth);
+  // const testDates = [
+  //   {
+  //     event: "sewing window",
+  //     fromDate: "2023-05-01",
+  //     toDate: "2023-05-13",
+  //   },
+  //   {
+  //     event: "harvesting window",
+  //     fromDate: "2023-06-11",
+  //     toDate: "2023-06-26",
+  //   },
+  // ];
+  // const CalendarSinglePlant = (): JSX.Element => {
+  //   const getMarked = () => {
+  //     let marked: Marked = {};
+  //     let color = "";
+  //     testDates.forEach(({ event, fromDate, toDate }) => {
+  //       if (event === "sewing window") {
+  //         color = "blue";
+  //       } else if (event === "harvesting window") {
+  //         color = "green";
+  //       }
+  //       const from = new Date(fromDate);
+  //       const to = new Date(toDate);
+  //       const currentDate = new Date(from);
+  //       while (currentDate <= to) {
+  //         const year = currentDate.getFullYear();
+  //         const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+  //         const day = String(currentDate.getDate()).padStart(2, "0");
+  //         if (!marked[`${year}-${month}-${day}`]) {
+  //           marked[`${year}-${month}-${day}`] = {
+  //             periods: [],
+  //           };
+  //         }
+  //         marked[`${year}-${month}-${day}`].periods.push({
+  //           startingDay: currentDate.getTime() === from.getTime(),
+  //           endingDay: currentDate.getTime() === to.getTime(),
+  //           color: color, // Set your desired color here
+  //         });
+  //         currentDate.setDate(currentDate.getDate() + 1);
+  //       }
+  //     });
+  //     console.log(marked);
+
+  //     return marked;
+  //   };
+  //   console.log(currentMonth);
   return (
     <SafeAreaView style={styles.container}>
       <Calendar
         initialDate={`${currentYear}-${currentMonth}-01`}
         markingType="multi-period"
         markedDates={getMarked()}
+        style={{
+          borderWidth: 1,
+          borderColor: "grey",
+          height: 500,
+        }}
       />
     </SafeAreaView>
   );
@@ -68,6 +122,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    width: "90%",
   },
 });
 export default CalendarSinglePlant;
