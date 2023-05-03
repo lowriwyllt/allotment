a9f3fa7f9033eab5ad6959103ef4c98be8875893
Peter Matthews <peterjmatthews@me.com>
2023-05-03 11:26:22 +0100

    Initial test render of NavDrawer in app level

--- App.tsx
+++ App.tsx
@@ -5,23 +5,26 @@ import HomeScreen from "./screens/HomeScreen/HomeScreen";
 import { RegisterScreen } from "./screens/RegisterScreen/RegisterScreen";
 import AllPlantsScreen from "./screens/AllPlantsScreen/AllPlantsScreen";
 import SinglePlantScreen from "./screens/SinglePlantScreen/SinglePlantScreen";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 import { NavDrawer } from "./screens/NavDrawer";
 
 const Stack = createNativeStackNavigator();
 
 export default function App(): JSX.Element {
-  return (<>
-  <NavigationContainer>
-  <NavDrawer/>
-      <Stack.Navigator>
-        <Stack.Screen name="login" component={LoginScreen} />
-        <Stack.Screen name="register" component={RegisterScreen} />
-        <Stack.Screen name="home" component={HomeScreen} />
-        <Stack.Screen name="plants" component={AllPlantsScreen} />
-        <Stack.Screen name="plant" component={SinglePlantScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+  return (
+    <>
+      <NavigationContainer>
+        <NavDrawer />
+      </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="login" component={LoginScreen} />
+          <Stack.Screen name="register" component={RegisterScreen} />
+          <Stack.Screen name="home" component={HomeScreen} />
+          <Stack.Screen name="plants" component={AllPlantsScreen} />
+          <Stack.Screen name="plant" component={SinglePlantScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
