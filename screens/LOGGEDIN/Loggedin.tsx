7cc11796330286c72ac68c1ee15177b0c368656f
Peter Matthews <peterjmatthews@me.com>
2023-05-04 11:05:48 +0100

    Remove unused code

--- screens/LOGGEDIN/Loggedin.tsx
+++ screens/LOGGEDIN/Loggedin.tsx
@@ -1,28 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet } from "react-native";
 import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import HomeScreen from "../HomeScreen/HomeScreen";
-import AllPlantsScreen from "../AllPlantsScreen/AllPlantsScreen";
-import SinglePlantScreen from "../SinglePlantScreen/SinglePlantScreen";
-import { NavDrawer } from "../NavDrawer";
+import { NavDrawer } from "../NavDrawer/NavDrawer";
 
 const Stack = createNativeStackNavigator();
 
 const Loggedin = () => {
   return (
-    // <NavigationContainer>
     <Stack.Navigator>
       <Stack.Screen
         name="Root"
         component={NavDrawer}
         options={{ headerShown: false }}
       />
-      {/* <Stack.Screen name="home" component={HomeScreen} />
-      <Stack.Screen name="plants" component={AllPlantsScreen} />
-      <Stack.Screen name="plant" component={SinglePlantScreen} /> */}
     </Stack.Navigator>
-    // </NavigationContainer>
   );
 };
 
