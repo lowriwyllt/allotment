81612a2974b05c4c099b74741afaf4657bc6c3cf
lowriwyllt <lowriwyllt@live.co.uk>
2023-05-05 09:40:33 +0100

    Edit the account page to cut out uneeded code

--- screens/Account/Account.tsx
+++ screens/Account/Account.tsx
@@ -1,20 +1,8 @@
-import {
-  View,
-  Text,
-  Image,
-  TextInput,
-  TouchableOpacity,
-  ScrollView,
-  KeyboardAvoidingView,
-  Modal,
-  Pressable,
-  Alert,
-} from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import { homeStyles } from "../HomeScreen/Home.component.style";
 import { useEffect, useState } from "react";
 import { getUserById } from "../../firebase/database";
 import LoginStyle from "../LoginScreen/Login.component.style";
-import EditProfileStyles from "./Account.component.style";
 import { useNavigation } from "@react-navigation/native";
 import { patchUser, getAvatars } from "../../firebase/database";
 import { getAuth, updateEmail, sendPasswordResetEmail } from "firebase/auth";
@@ -25,12 +13,12 @@ export default function Account({
 }: any): JSX.Element {
   console.log(currentUser);
 
-  const [avatarUrl, setAvatarUrl] = useState<string | undefined>("");
+  const [avatarUrl, setAvatarUrl] = useState<string | undefined>(
+    "www.blah.com"
+  );
   const [name, setName] = useState<string | undefined>("");
   const [email, setEmail] = useState<string | null | undefined>("");
   const [oldEmail, setOldEmail] = useState<string | null | undefined>("");
-  const [modalVisible, setModalVisible] = useState<boolean | undefined>(false);
-  const [avatarsArr, setAvatarsArr] = useState<string[] | undefined>([]);
 
   const navigation = useNavigation<any>();
   const auth = getAuth();
@@ -49,30 +37,6 @@ export default function Account({
       });
   }, []);
 
-  useEffect(() => {
-    getAvatars().then((res) => {
-      setAvatarsArr(res);
-    });
-  }, []);
-
-  const handleSubmit = (
-    name: string | undefined,
-    email: string,
-    avatarUrl: string
-  ) => {
-    const user: any = auth.currentUser;
-
-    updateEmail(user, email).catch((err) => {
-      console.log(err);
-    });
-
-    patchUser(currentUser, name, email, avatarUrl);
-    setCurrentUserEmail(email);
-    navigation.replace("home");
-  };
-
-  const changeAvatar = () => {};
-
   return (
     <ScrollView>
       <View style={LoginStyle.avatarsContainer}>
@@ -83,8 +47,8 @@ export default function Account({
         ></Image>
       </View>
       <View style={homeStyles.container}>
-        <Text>{name}</Text>
-        <Text>{email}</Text>
+        <Text>Name: {name}</Text>
+        <Text>Email: {email}</Text>
 
         <TouchableOpacity onPress={() => navigation.navigate("editAccount")}>
           <Text>Edit account details</Text>
