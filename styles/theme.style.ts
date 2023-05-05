408ffad767d4a3bc5795e32fc95b249145a0d813
Peter Matthews <peterjmatthews@me.com>
2023-05-05 11:24:58 +0100

    Add new color to theme styles

--- styles/theme.style.ts
+++ styles/theme.style.ts
@@ -2,8 +2,9 @@ const theme = {
   green: "#158C66",
   darkgreen: "#183032",
   cream: "#FFECCC",
+  lightcream: "#fff9ee"
   orange: "#EE5D1F",
-  brown: "84432A",
+  brown: "#84432A",
 };
 
 export default theme;
