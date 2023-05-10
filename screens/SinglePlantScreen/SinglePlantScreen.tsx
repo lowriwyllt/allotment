81495dd687e69d88b2a3471e7b4e18a057870709
Peter Matthews <peterjmatthews@me.com>
2023-05-10 16:45:07 +0100

    extract SinglePlantsScreen styles

--- screens/SinglePlantScreen/SinglePlantScreen.tsx
+++ screens/SinglePlantScreen/SinglePlantScreen.tsx
@@ -18,6 +18,7 @@ import CalendarSinglePlant from "./components/Calendar";
 import theme from "../../styles/theme.style";
 import DateModal from "./components/DateModal";
 import { useIsFocused } from "@react-navigation/native";
+import { SinglePlantStyles } from "../../styles/singlePlantsScreen.style";
 
 //--------------------------------need to change any----------------------------------------
 const SinglePlantScreen = ({ route, currentUser }: any) => {
@@ -151,71 +152,3 @@ const SinglePlantScreen = ({ route, currentUser }: any) => {
 
 export default SinglePlantScreen;
 
-export const SinglePlantStyles = StyleSheet.create({
-  header: {
-    fontSize: theme.mainheader,
-    fontWeight: "700",
-    textAlign: "center",
-    marginTop: 20,
-  },
-  plantImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    resizeMode: "cover",
-  },
-  container: {
-    flex: 1,
-    alignItems: "center",
-    backgroundColor: theme.lightcream,
-    // padding: 50,
-  },
-  button: {
-    backgroundColor: theme.green, // chose green as original was feature which doesnt exist
-    width: 40,
-    height: 40,
-    justifyContent: "center",
-    borderRadius: 20,
-    alignItems: "center",
-    marginTop: 40,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-  centeredView: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 22,
-  },
-  modalView: {
-    width: "80%",
-    margin: 20,
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
-  // buttonClose: {
-  //   backgroundColor: "#2196F3",
-  // },
-});
---
+++ styles/singlePlantsScreen.style.ts
@@ -0,0 +1,72 @@
+import { StyleSheet } from "react-native";
+import theme from "./theme.style";
+
+export const SinglePlantStyles = StyleSheet.create({
+    header: {
+      fontSize: theme.mainheader,
+      fontWeight: "700",
+      textAlign: "center",
+      marginTop: 20,
+    },
+    plantImage: {
+      width: 100,
+      height: 100,
+      borderRadius: 50,
+      resizeMode: "cover",
+    },
+    container: {
+      flex: 1,
+      alignItems: "center",
+      backgroundColor: theme.lightcream,
+      // padding: 50,
+    },
+    button: {
+      backgroundColor: theme.green, // chose green as original was feature which doesnt exist
+      width: 40,
+      height: 40,
+      justifyContent: "center",
+      borderRadius: 20,
+      alignItems: "center",
+      marginTop: 40,
+    },
+    buttonText: {
+      color: "white",
+      fontWeight: "700",
+      fontSize: 16,
+    },
+    centeredView: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 22,
+    },
+    modalView: {
+      width: "80%",
+      margin: 20,
+      backgroundColor: "white",
+      borderRadius: 20,
+      padding: 35,
+      alignItems: "center",
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    textStyle: {
+      color: "white",
+      fontWeight: "bold",
+      textAlign: "center",
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: "center",
+    },
+    // buttonClose: {
+    //   backgroundColor: "#2196F3",
+    // },
+  });
+  
